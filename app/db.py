7a4e07362f6606c660577deb22e352db55f36915
pavel-2009 <ms.radionova@bk.ru>
2026-02-24 14:49:00 +0500

    need to rewrite CRUD into Alchemy&Alembic && create basic CV-ops

---
+++ app/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+
+from typing import AsyncGenerator
+
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+Base = declarative_base()
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    engine = create_async_engine(DATABASE_URL, echo=True)
+    
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
--- app/models.py
+++ app/models.py
@@ -3,8 +3,12 @@ Pydantic модели для валидации
 """
 
 from pydantic import BaseModel, Field
+from sqlalchemy import Column, Integer, String
+
+from app.db import Base
 
 
+# === Pydantic модели ===
 class TaskBase(BaseModel):
     """Базовый класс задачи"""
     title: str = Field(min_length=1, max_length=20, description="Название задачи")
@@ -24,3 +28,13 @@ class TaskUpdate(TaskBase):
 class TaskGet(TaskBase):
     """Класс для получения задачи"""
     id: int
+
+
+# === SQLAlchemy модели ===
+class Task(Base):
+    """Модель задачи для базы данных"""
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(20), nullable=False)
+    description = Column(String(400), nullable=True)
