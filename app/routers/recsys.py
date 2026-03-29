cb0fc61c5626d54c6ef23a4aaa1e96ca39bd9b23
pavel-2009 <ms.radionova@bk.ru>
2026-03-29 13:54:25 +0500

    implemnted api-router for cf recommendations

--- app/routers/recsys.py
+++ app/routers/recsys.py
@@ -11,6 +11,7 @@ from app.schemas import Recommendation, RecommendationGet
 from app.db import get_async_session
 from app.auth import get_current_user
 from app.ml.recsys.content_based import ContentBasedRecommender
+from app.ml.recsys.collaborative_filtering import CollaborativeFilteringRecommender
 
 
 router = APIRouter(
@@ -55,4 +56,34 @@ async def get_recommendations(
             for rec in recommendations[:top_k]  # Ограничиваем количество рекомендаций до top_k
         ]
     )
+    
+
+@router.get("/cf/recommendations", response_model=RecommendationGet, status_code=status.HTTP_200_OK)
+async def get_cf_recommendations(
+    current_user: User = Depends(get_current_user),
+    top_k: int = Query(5, ge=1, le=20),
+    session: AsyncSession = Depends(get_async_session)
+) -> RecommendationGet:
+    """Получение рекомендаций на основе коллаборативной фильтрации для текущего пользователя."""
+    
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Пользователь не аутентифицирован"
+        )
+    
+    collaborative_filtering_recommender: CollaborativeFilteringRecommender = services.get_service("collaborative_filtering_recommender")
+    
+    recommendations = await collaborative_filtering_recommender.recommend(current_user.id, session, top_k=top_k)
+    
+    return RecommendationGet(
+        recommendations=[
+            Recommendation(
+                task_id=rec["task_id"],
+                description=rec["description"],
+                similarity_score=rec["similarity_score"]
+            )
+            for rec in recommendations[:top_k]  # Ограничиваем количество рекомендаций до top_k
+        ]   
+)
     
