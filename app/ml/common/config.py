1f5c474c5961cbbf42cfc67c842c8f81eda60e8e
Павел <ms.radionova@bk.ru>
2026-04-04 19:24:18 +0500

    Update config.py

--- app/ml/common/config.py
+++ app/ml/common/config.py
@@ -16,7 +16,7 @@ class MLConfig(BaseSettings):
     img_size: int = 224
     batch_size: int = 16
     num_workers: int = 2
-    num_classes: int = 1000
+    num_classes: int = 3
     mean: list[float] = [0.485, 0.456, 0.406]
     std: list[float] = [0.229, 0.224, 0.225]
     learning_rate: float = 0.001
